18b8d0f5b016fe58d8085de7c7e702862deb9ff7
Aaron Walker <aaronpwalker@gmail.com>
2018-06-04 13:09:03 +1000

    very basic ecs component

---
+++ ecs-services.cfndsl.rb
@@ -0,0 +1,223 @@
+CloudFormation do
+
+  log_retention = 7 unless defined?(log_retention)
+  Resource('LogGroup') {
+    Type 'AWS::Logs::LogGroup'
+    Property('LogGroupName', Ref('AWS::StackName'))
+    Property('RetentionInDays', "#{log_retention}")
+  }
+
+  definitions, task_volumes = Array.new(2){[]}
+
+  task_definition.each do |task_name, task|
+
+    env_vars, mount_pounts, ports = Array.new(3){[]}
+
+    name = task.has_key?('name') ? task['name'] : task_name
+
+    image_repo = task.has_key?('repo') ? "#{task['repo']}/" : ''
+    image_name = task.has_key?('image') ? task['image'] : task_name
+    image_tag = task.has_key?('tag') ? "#{task['tag']}" : 'latest'
+    image_tag = task.has_key?('tag_param') ? Ref("#{task['tag_param']}") : image_tag
+
+    # create main definition
+    task_def =  {
+      Name: name,
+      Image: FnJoin('',[ image_repo, image_name, ":", image_tag ]),
+      LogConfiguration: {
+        LogDriver: 'awslogs',
+        Options: {
+          'awslogs-group' => Ref("LogGroup"),
+          "awslogs-region" => Ref("AWS::Region"),
+          "awslogs-stream-prefix" => name
+        }
+      }
+    }
+
+    task_def.merge!({ MemoryReservation: task['memory'] }) if task.has_key?('memory')
+    task_def.merge!({ Cpu: task['cpu'] }) if task.has_key?('cpu')
+
+    if !(task['env_vars'].nil?)
+      task['env_vars'].each do |name,value|
+        split_value = value.to_s.split(/\${|}/)
+        if split_value.include? 'environment'
+          fn_join = split_value.map { |x| x == 'environment' ? [ Ref('EnvironmentName'), '.', FnFindInMap('AccountId',Ref('AWS::AccountId'),'DnsDomain') ] : x }
+          env_value = FnJoin('', fn_join.flatten)
+        elsif value == 'cf_version'
+          env_value = cf_version
+        else
+          env_value = value
+        end
+        env_vars << { Name: name, Value: env_value}
+      end
+    end
+
+    task_def.merge!({Environment: env_vars }) if env_vars.any?
+
+    # add links
+    if task.key?('links')
+      task['links'].each do |links|
+      task_def.merge!({ Links: [ links ] })
+      end
+    end
+
+    # add entrypoint
+    if task.key?('entrypoint')
+      task['entrypoint'].each do |entrypoint|
+      task_def.merge!({ EntryPoint: entrypoint })
+      end
+    end
+
+    # By default Essential is true, switch to false if `not_essential: true`
+    task_def.merge!({ Essential: false }) if task['not_essential']
+
+    # add docker volumes
+    if (task.key?('mounts'))
+      task['mounts'].each do |mount|
+        path = (mount.key?('path') ? mount['path'] :  tasks[service]['volumes'][mount['volume']])
+        mount_pounts << { ContainerPath: path, SourceVolume: mount['volume'], ReadOnly: (mount.key?('ReadOnly') ? true : false) }
+      end
+      task_def.merge!({MountPoints: mount_pounts })
+    end
+
+    # add port
+    if task.key?('ports')
+      port_mapppings = []
+      task['ports'].each do |port|
+        port_array = port.to_s.split(":").map(&:to_i)
+        mapping = {}
+        mapping.merge!(ContainerPort: port_array[0])
+        mapping.merge!(HostPort: port_array[1]) if port_array.length == 2
+        port_mapppings << mapping
+      end
+      task_def.merge!({PortMappings: port_mapppings})
+    end
+
+    definitions << task_def
+
+  end
+
+  # add docker volumes
+  if defined?(volumes)
+    volumes.each do |volume, path|
+      task_volumes << { Name: volume, Host: { SourcePath: path } }
+    end
+  end
+
+  if defined?(iam)
+    IAM_Role('TaskRole') do
+      AssumeRolePolicyDocument ({
+        Statement: [
+          {
+            Effect: 'Allow',
+            Principal: { Service: [ 'ec2.amazonaws.com' ] },
+            Action: [ 'sts:AssumeRole' ]
+          },
+          {
+            Effect: 'Allow',
+            Principal: { Service: [ 'ssm.amazonaws.com' ] },
+            Action: [ 'sts:AssumeRole' ]
+          }
+        ]
+      })
+      Path '/'
+      Policies(IAMPolicies.new.create_policies(iam))
+    end
+
+    IAM_Role('ExecutionRole') do
+      AssumeRolePolicyDocument ({
+        Statement: [
+          {
+            Effect: 'Allow',
+            Principal: { Service: [ 'ecs-tasks.amazonaws.com' ] },
+            Action: [ 'sts:AssumeRole' ]
+          }
+        ]
+      })
+      Path '/'
+      Policies({
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Effect: "Allow",
+            Action: [
+              "ecr:GetAuthorizationToken",
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:BatchGetImage",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents"
+            ],
+            Resource: "*"
+          }
+        ]
+      })
+    end
+  end
+
+  Resource('Task') do
+    Type 'AWS::ECS::TaskDefinition'
+    Property('ContainerDefinitions', definitions)
+
+    if defined?(cpu)
+      Property('Cpu', cpu)
+    end
+
+    if defined?(memory)
+      Property('Memory', memory)
+    end
+
+    if defined?(network_mode)
+      Property('NetworkMode', network_mode)
+    end
+
+    if task_volumes.any?
+      Property('Volumes', task_volumes)
+    end
+
+    if defined?(iam)
+      Property('TaskRoleArn', Ref('TaskRole'))
+      Property('ExecutionRoleArn', Ref('ExecutionRole'))
+    end
+
+  end
+
+  service_loadbalancer = []
+  if targetgroup
+    service_loadbalancer << {
+      ContainerName: targetgroup['container'],
+      ContainerPort: targetgroup['port'],
+      TargetGroupArn: Ref("#{targetgroup['name']}TargetGroup")
+    }
+  end
+
+  IAM_Role('Role') do
+    AssumeRolePolicyDocument ({
+      Statement: [
+        Effect: 'Allow',
+        Principal: { Service: [ 'ecs.amazonaws.com' ] },
+        Action: [ 'sts:AssumeRole' ]
+      ]
+    })
+    Path '/'
+    Policies Policies(IAMPolicies.new.create_policies([
+      'ecs-service-role'
+    ]))
+  end
+
+  ECS_Service('Service') do
+    Cluster Ref("EcsCluster")
+    DesiredCount 1
+    DeploymentConfiguration ({
+        MinimumHealthyPercent: 100,
+        MaximumPercent: 200
+    })
+    TaskDefinition Ref('Task')
+
+    if service_loadbalancer.any?
+      Role Ref('Role')
+      LoadBalancers service_loadbalancer
+    end
+  end
+
+end
---
+++ ecs-services.highlander.rb
@@ -0,0 +1,25 @@
+HighlanderComponent do
+
+  DependsOn 'vpc@1.0.4'
+
+  Parameters do
+    StackParam 'EnvironmentName', 'dev', isGlobal: true
+    StackParam 'EnvironmentType', 'development', isGlobal: true
+    StackParam 'Tag', 'latest'
+    
+    OutputParam component: 'vpc', name: "VPCId"
+    OutputParam component: 'loadbalancer', name: 'LoadBalancer'
+    OutputParam component: 'ecs', name: 'EcsCluster'
+    OutputParam component: 'loadbalancer', name: "#{targetgroup['name']}TargetGroup"
+
+    #create component params for service image tag parameters
+    task_definition.each do |task_def, task|
+      if task.has_key?('tag_param')
+        default_value = task.has_key?('tag_param_default') ? task['tag_param_default'] : 'latest'
+        StackParam task['tag_param'], default_value
+      end
+    end
+
+  end
+  
+end
